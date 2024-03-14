e6789b70d67b45f1af611f767513afe2965f2db5
cris <carincon93@gmail.com>
2024-03-14 08:54:47 -0500

    New Dims added

--- Sources/OBSERVATORIO_MUNICIPOS.sql
+++ Sources/OBSERVATORIO_MUNICIPOS.sql
@@ -30,6 +30,74 @@ CREATE TABLE [dbo].[Dim_Municipio](
 ) ON [PRIMARY]
 GO
 
+/****** Object:  Table [dbo].[Dim_Anio] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Dim_Anio](
+	[Anio_SKey] [int] IDENTITY(1,1) NOT NULL,
+	[Anio] [int] NOT NULL,
+ CONSTRAINT [PK_Anio] PRIMARY KEY CLUSTERED 
+(
+	[Anio_SKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Dim_Mes] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Dim_Mes](
+	[Mes_SKey] [int] IDENTITY(1,1) NOT NULL,
+	[Mes] [varchar](20) NOT NULL,
+ CONSTRAINT [PK_Mes] PRIMARY KEY CLUSTERED 
+(
+	[Mes_SKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Dim_Genero] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Dim_Genero](
+	[Genero_SKey] [int] IDENTITY(1,1) NOT NULL,
+	[Genero] [varchar](10) NOT NULL,
+ CONSTRAINT [PK_Genero] PRIMARY KEY CLUSTERED 
+(
+	[Genero_SKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Dim_Agrupacion] ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Dim_Agrupacion](
+	[Agrupacion_SKey] [int] IDENTITY(1,1) NOT NULL,
+	[Agrupacion] [varchar](20) NOT NULL,
+ CONSTRAINT [PK_Agrupacion] PRIMARY KEY CLUSTERED 
+(
+	[Agrupacion_SKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 /****** Object:  Table [dbo].[Dim_Indicador] ******/
 SET ANSI_NULLS ON
 GO
@@ -75,16 +143,20 @@ CREATE TABLE [dbo].[Municipio_Stats](
 	[Municipio_SKey] [int] NOT NULL,
 	[Indicador_SKey] [int] NOT NULL,
 	[Categoria_SKey] [int] NOT NULL,
-	[Genero] [varchar](10) NULL,
-	[Agrupacion] [varchar](20) NULL,
+	[Genero_SKey] [int] NOT NULL,
+	[Agrupacion_SKey] [int] NOT NULL,
+	[Anio_SKey] [int] NOT NULL,
+	[Mes_SKey] [int] NOT NULL,
 	[Valor]  [decimal](10, 8) NOT NULL,
-	[Anio] [int] NOT NULL,
-	[Mes] [varchar](20) NULL,
  CONSTRAINT [PK_Municipio_Stats] PRIMARY KEY CLUSTERED 
 (
 	[Municipio_SKey] ASC,
 	[Indicador_SKey] ASC,
-	[Categoria_SKey] ASC
+	[Categoria_SKey] ASC,
+	[Genero_SKey] ASC,
+	[Agrupacion_SKey] ASC,
+	[Anio_SKey] ASC,
+	[Mes_SKey] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
@@ -108,5 +180,33 @@ REFERENCES [dbo].[Dim_Categoria] ([Categoria_SKey])
 GO
 
 ALTER TABLE [dbo].[Municipio_Stats] CHECK CONSTRAINT [FK_Municipio_Stats_Dim_Categoria]
+GO
+
+ALTER TABLE [dbo].[Municipio_Stats]  WITH CHECK ADD  CONSTRAINT [FK_Municipio_Stats_Dim_Genero] FOREIGN KEY([Genero_SKey])
+REFERENCES [dbo].[Dim_Genero] ([Genero_SKey])
+GO
+
+ALTER TABLE [dbo].[Municipio_Stats] CHECK CONSTRAINT [FK_Municipio_Stats_Dim_Genero]
+GO
+
+ALTER TABLE [dbo].[Municipio_Stats]  WITH CHECK ADD  CONSTRAINT [FK_Municipio_Stats_Dim_Agrupacion] FOREIGN KEY([Agrupacion_SKey])
+REFERENCES [dbo].[Dim_Agrupacion] ([Agrupacion_SKey])
+GO
+
+ALTER TABLE [dbo].[Municipio_Stats] CHECK CONSTRAINT [FK_Municipio_Stats_Dim_Agrupacion]
+GO
+
+ALTER TABLE [dbo].[Municipio_Stats]  WITH CHECK ADD  CONSTRAINT [FK_Municipio_Stats_Dim_Anio] FOREIGN KEY([Anio_SKey])
+REFERENCES [dbo].[Dim_Anio] ([Anio_SKey])
+GO
+
+ALTER TABLE [dbo].[Municipio_Stats] CHECK CONSTRAINT [FK_Municipio_Stats_Dim_Anio]
+GO
+
+ALTER TABLE [dbo].[Municipio_Stats]  WITH CHECK ADD  CONSTRAINT [FK_Municipio_Stats_Dim_Mes] FOREIGN KEY([Mes_SKey])
+REFERENCES [dbo].[Dim_Mes] ([Mes_SKey])
+GO
+
+ALTER TABLE [dbo].[Municipio_Stats] CHECK CONSTRAINT [FK_Municipio_Stats_Dim_Mes]
 GO
 
